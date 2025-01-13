89ffdaf9724873ddc4aa6f1daf73cc350fca534c
rajnath10009 <raj.nath@g10x.com>
2025-01-13 10:54:26 +0530

    new --v

--- Connect.py
+++ Connect.py
@@ -20,8 +20,7 @@ class Asset(SQLModel, table=True):  # Specify table=True for ORM mapping
     asset_purchasedate: Optional[datetime] = Field(default_factory=datetime.now)
     asset_warranty_end: Optional[datetime] = Field(default_factory=datetime.now)
     asset_status: str = Field(nullable=False, max_length=50)
-    quantity_available: int = Field(nullable=False)
-    quantity_allocated: int = Field(nullable=False)
+    allocated_to: int = Field(nullable=False, foreign_key="employee.employee_id")
  
 class Notification(SQLModel, table=True):  # Specify table=True for ORM mapping
     notification_id: Optional[int] = Field(default=None, primary_key=True)  # Removed autoincrement
@@ -31,5 +30,15 @@ class Notification(SQLModel, table=True):  # Specify table=True for ORM mapping
     sent_at: Optional[datetime] = Field(default_factory=datetime.now)
     priority: int = Field(nullable=False)
  
- 
- 
+class Ticket(SQLModel, table=True):
+    __tablename__ = 'Ticket'
+    ticket_id: Optional[int] = Field(default=None, primary_key=True)
+    employee_id: Optional[int] = Field(default=None, foreign_key='employee.employee_id')
+    asset_id: Optional[int] = Field(default=None, foreign_key='asset.asset_id')
+    ticket_type: str = Field(nullable=False, max_length=50)
+    ticket_status: Optional[int] = Field(default=None, foreign_key='notification.notification_id')
+    assigned_date: Optional[datetime] = Field(default=datetime.today)
+    return_date: Optional[datetime] = Field(default=datetime.today)
+    returned_condition: str = Field(nullable=True, max_length=255)
+    raised_at: Optional[datetime] = Field(default=datetime.now)
+    resolved_at: Optional[datetime] = Field(default=datetime.now)
